508d29d281a4310c145fa4376a946d5d81c94eee
Roman-Zhirovskis <tesslogun@gmail.com>
2023-04-05 09:12:56 +0300

    linter settings

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path("", views.HomeTempaltes.as_view(), name="home"),
 ]
