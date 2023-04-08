da70320ef871b6759ab0705704d32ae0b09c5f72
Roman-Zhirovskis <tesslogun@gmail.com>
2023-04-08 11:24:07 +0300

    try 5

--- tests/tests_model.py
+++ tests/tests_model.py
@@ -9,3 +9,8 @@ def test_women_str_method(women):
 @pytest.mark.django_db
 def test_category_str_method(category):
     assert str(category) == "Test Category"
+
+
+@pytest.mark.django_db
+def test_category_str_method1(category):
+    assert str(category) == "Test Category2"
